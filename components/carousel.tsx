8e2f442342f1e99fd4d54e5772c83f2c6768683f
RealRajnish <rajnishf3@gmail.com>
2023-12-25 23:44:54 +0530

    fix: unnecessary component removed

--- components/carousel.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Carousel({ children }) {
-  return (
-    <div className="overflow-hidden relative">
-      <div className="flex">{children}</div>
-    </div>
-  );
-}
